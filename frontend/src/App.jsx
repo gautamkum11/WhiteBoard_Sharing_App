8447c186a02023583a290ce458383745be68f15b
Gautam <kumawatgautam305@gmail.com>
2023-08-26 19:36:03 +0530

    add port

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import io from "socket.io-client";
 import { useEffect, useState } from 'react';
 import {toast, ToastContainer} from "react-toastify";
 
-const server = "http://localhost:5000";
+const server = "https://64ea0479f3c7703c092dc88c--incredible-sundae-50b18a.netlify.app/";
 const connectionOptions = {
   "force new connection": true,
   reconnectionAttempts: "Infinity",
